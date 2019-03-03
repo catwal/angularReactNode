aefe23d6d2da5dbbb8b17a278d0ba30184b23693
catwal <kitekate.cw@gmail.com>
2019-03-03 15:57:37 +0100

    ajout du serveur mongoose

--- server/index.js
+++ server/index.js
@@ -1,5 +1,20 @@
+//c'est le point d'entrer du serveur
+
+
 /* import des packages */
 const express = require('express');
+const mongoose = require('mongoose');
+
+//import des fichiers exterieurs
+const config= require('./config/dev');
+const rental = require('./models/schemaRental');
+const FakeDb = require('./fake-db');
+
+//Mongoose URI - connect est une promise
+mongoose.connect(config.DB_URI).then(()=>{
+  const fakeDb = new FakeDb();
+  fakeDb.seedDb();
+});
 
 const app = express();
 // specification d'un endpoint
@@ -12,10 +27,3 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, function(){
   console.log('I am running');
 });
-
-/*
-app.listen(3001, function(){
-  console.log('I am running');
-
-})
- */
